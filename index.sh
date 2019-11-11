8d105256d60402483fdaf9809e984eed42754687
Kelly Crowther <crowther.kelly1@gmail.com>
2019-11-11 10:53:47 -0800

    finalize getting ping results and appending to csv

--- index.sh
+++ index.sh
@@ -3,9 +3,12 @@
 # send 5 pings to google and write the output to log.txt
 ping -c 5 google.com > log.txt
 
-# find the line with stddev and cut out the min, avg, max, and stddev and append to log.csv
-grep -i "stddev" log.txt | cut -d " " -f 4 >> log.csv
+# find the line with stddev, cut out the min, avg, max, and stddev, and replace the slashes with commas
+pingResults=$(grep -i "stddev" log.txt | cut -d " " -f 4 | sed 's/\//,/g')
+
+# append the results to the log.csv file
+echo $pingResults >> log.csv
 
 # empty backup file extension required on OSX using -i flag ex: sed -i '' ...
-# replace slashes with commas
-sed -i '' 's/\//,/g' log.csv
+# sed -i '' 's/\//,/g' tmp.txt
+
